f746bc298d884d25deba4e41a5d5fb8d939661d9
mohammadAyan1 <mohammadayan2210@gmail.com>
2025-09-29 16:21:18 +0530

    make some changes for the token

--- server/middleware/auth.middleware.js
+++ server/middleware/auth.middleware.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken")
 
 const protectedRoutes = async (req, res, next) => {
   try {
-    const token = req?.cookies?.token;
+    const {token} = req.cookies;
     console.log(req);
     
     console.log(token,"jhgfhg");
